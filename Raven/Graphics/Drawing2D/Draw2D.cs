8a0c3ef88e46b6e6e13d0a90b6618808ec79a4d0
Stencho <c3142113@gmail.com>
2026-03-31 13:55:13 +1100

    Color interpolation no longer ignores alpha

--- Raven/Graphics/Drawing2D/Draw2D.cs
+++ Raven/Graphics/Drawing2D/Draw2D.cs
@@ -122,13 +122,14 @@ public static class Draw2D {
     /// <param name="colorB">The second color</param>
     /// <param name="bAmount">The amount to interpolate; 0.0 for 100% color A, 1.0 for color B</param>
     /// <returns>The resulting Color</returns>
-    public static Color ColorInterpolate(Color colorA, Color colorB, float bAmount) {
+    public static Color ColorInterpolate(Color colorA, Color colorB, double bAmount) {
         var aAmount = 1.0f - bAmount;
         var r = (int)(colorA.R * aAmount + colorB.R * bAmount);
         var g = (int)(colorA.G * aAmount + colorB.G * bAmount);
         var b = (int)(colorA.B * aAmount + colorB.B * bAmount);
+        var a = (int)(colorA.A * aAmount + colorB.A * bAmount);
 
-        return Color.FromNonPremultiplied(r, g, b, 255);
+        return Color.FromNonPremultiplied(r, g, b, a);
     }
 
     /// <summary>
