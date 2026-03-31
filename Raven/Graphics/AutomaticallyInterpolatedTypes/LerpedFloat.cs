ffa12e6d67402c0ae573f183efcace49b579d90d
Stencho <c3142113@gmail.com>
2026-03-31 13:56:27 +1100

    Added little manual float lerping class for use mostly with the UI. makes it mad easy to do little color lerps on mouse over and such

--- Raven/Graphics/AutomaticallyInterpolatedTypes/LerpedFloat.cs
+++ Raven/Graphics/AutomaticallyInterpolatedTypes/LerpedFloat.cs
@@ -1,7 +1,12 @@
+using System;
+using Raven.Engine;
+
 namespace Raven.Graphics.InterpolatedTypes;
 
 public class LerpedFloat : AutoInterpolate<float> {
-    public LerpedFloat(float start_value, float end_value, double length_ms, InterpolationType interpolation_type = InterpolationType.Loop, InterpolationThread interpolation_thread = InterpolationThread.Render) 
+    public LerpedFloat(float start_value, float end_value, double length_ms, 
+        InterpolationType interpolation_type = InterpolationType.Loop, 
+        InterpolationThread interpolation_thread = InterpolationThread.Render) 
         : base(start_value, end_value, length_ms, interpolation_type, interpolation_thread) {
         init();
     }
@@ -12,5 +17,71 @@ public class LerpedFloat : AutoInterpolate<float> {
     
     float tween(float start, float end, double progress) {
         return float.Lerp(start, end, (float)progress);
+    }
+}
+
+public class FloatLerperManual {
+    private float start = 0;
+    private float end = 0;
+
+    private float length_ms = 0;
+    private float position_ms = 0;
+
+    private float lerp_position = 0;
+
+    public float Start  => start;
+    public float End => end;
+    
+    public float Value =>  float.Lerp(start, end, lerp_position);
+
+
+    private InterpolationType InterpolationType { get; set; } = InterpolationType.Once;
+    private InterpolationThread Thread { get; set; } = InterpolationThread.Render;
+    
+    public FloatLerperManual(float start_value, float end_value, float length_ms, 
+        InterpolationType interpolation_type = InterpolationType.Once, 
+        InterpolationThread interpolation_thread  = InterpolationThread.Render) {
+        
+        start = start_value;
+        end = end_value;
+        
+        InterpolationType = interpolation_type;
+        Thread = interpolation_thread;
+        
+        this.length_ms = length_ms;
+    }
+            
+    public void Lerp() {
+        var delta = Thread == InterpolationThread.Render ? Clock.delta_time_ms_f : (float)State.scene_update_thread.delta_ms;
+        position_ms += delta;
+        lerp();
+    }
+    public void LerpReverse() {
+        var delta = Thread == InterpolationThread.Render ? Clock.delta_time_ms_f : (float)State.scene_update_thread.delta_ms;
+        position_ms -= delta;
+        lerp();
+    }
+
+    void lerp() {
+        switch (InterpolationType) {
+            case InterpolationType.Once:
+                position_ms = float.Clamp(position_ms, 0f, length_ms);
+                lerp_position = float.Clamp(position_ms / length_ms, 0f, 1f);
+                break;
+            
+            case InterpolationType.Loop:
+                lerp_position = (MathF.Abs(position_ms) % length_ms) / length_ms;
+                break;
+            
+            case InterpolationType.Bounce:
+                var flipflop = (int)(MathF.Abs(position_ms) / length_ms) % 2 == 0;
+                if (flipflop) lerp_position = (MathF.Abs(position_ms) % length_ms) / length_ms;
+                else lerp_position = 1f - (MathF.Abs(position_ms) % length_ms) / length_ms;
+                break;
+            
+            case InterpolationType.OnceEvery: throw new NotImplementedException();
+            case InterpolationType.OnceEveryRandom: throw new NotImplementedException();
+            default: throw new ArgumentOutOfRangeException();
+        }
     }
 }
