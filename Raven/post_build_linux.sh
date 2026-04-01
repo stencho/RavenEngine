b6a901c8d9b687642dd760aa898475f514ba2f30
Stencho <c3142113@gmail.com>
2026-04-01 13:32:27 +1100

    post-build system modified slightly. Going to attempt windows handling as well.

--- Raven/post_build_linux.sh
+++ Raven/post_build_linux.sh
@@ -3,5 +3,5 @@ project_names=("Cassowary")
 
 for name in "${project_names[@]}"; do 
 	echo "Copying engine content to $name"
-	rsync -avh --progress --partial --inplace Raven/Content/bin/DesktopGL/Content/ $name/bin/Debug/net9.0/Content/
+	rsync -avh --progress --partial --inplace Content/bin/DesktopGL/Content/ ../$name/bin/Debug/net9.0/Content/
 done;
